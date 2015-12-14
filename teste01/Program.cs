4659998aa4ccb72c719cbf8681b52a1738241e94
kenio natan <kenio_natan@hotmail.com>
2015-12-14 08:56:52 -0300

    Start Commit

---
+++ teste01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var geo = new Address();
+            geo.Line1 = "30 Frank Lloyd Wright Drive";
+            geo.State = "MI";
+
+            geo.GeoCode();
+
+            
+
+            Console.WriteLine("Teste");
+            Console.WriteLine(geo.Latitude);
+            Console.WriteLine(geo.Longitude);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
